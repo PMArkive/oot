80e333fdf4fdd70d37de7d798330beacc793bc4a
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-04-20 11:42:38 +0200

    rm debugging stuff from gha_fix_bss_and_generate_patch.sh

--- tools/gha_fix_bss_and_generate_patch.sh
+++ tools/gha_fix_bss_and_generate_patch.sh
@@ -5,14 +5,7 @@ echo "Build failed, attempting to fix BSS ordering..."
 
 .venv/bin/python3 tools/fix_bss.py -v ${VERSION}
 
-pwd
-git status
-git diff
-
 PATCH=$(git diff | base64 -w 0)
-
-echo $PATCH
-
 if [ -n "$PATCH" ]; then
     echo "\`\`\`" >> $GITHUB_STEP_SUMMARY
     echo "Fixes were made for your PR. To apply these changes to your working directory," >> $GITHUB_STEP_SUMMARY
