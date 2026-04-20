bfb7eb874e1a5fb94b33972a6af95fa2e74d7e36
Dragorn421 <Dragorn421@users.noreply.github.com>
2026-04-20 10:54:41 +0200

    debugging gha_fix_bss_and_generate_patch.sh

--- tools/gha_fix_bss_and_generate_patch.sh
+++ tools/gha_fix_bss_and_generate_patch.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "\`\`\`"
 echo "Build failed, attempting to fix BSS ordering..."
 
 .venv/bin/python3 tools/fix_bss.py -v ${VERSION}
 
+echo $pwd
+git status
+git diff
+
 PATCH=$(git diff | base64 -w 0)
+
+echo $PATCH
+
 if [ -n "$PATCH" ]; then
-    echo "Fixes were made for your PR. To apply these changes to your working directory,"
-    echo "copy and run the following command:"
-    echo
-    echo "echo -n $PATCH | base64 -d | git apply -"
-    echo
+    echo "\`\`\`" >> $GITHUB_STEP_SUMMARY
+    echo "Fixes were made for your PR. To apply these changes to your working directory," >> $GITHUB_STEP_SUMMARY
+    echo "copy and run the following command:" >> $GITHUB_STEP_SUMMARY
+    echo >> $GITHUB_STEP_SUMMARY
+    echo "echo -n $PATCH | base64 -d | git apply -" >> $GITHUB_STEP_SUMMARY
+    echo >> $GITHUB_STEP_SUMMARY
+    echo "\`\`\`" >> $GITHUB_STEP_SUMMARY
 fi
-echo "\`\`\`"
 
 exit 1
